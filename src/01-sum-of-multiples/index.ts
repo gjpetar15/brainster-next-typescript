d6b8717754feaf237b0d2edf75a8baa16589655b
Petar Gjorgjioski <gjorgjioski.a22011@next.edu.mk>
2023-12-21 12:19:20 +0100

    first exercise

--- src/01-sum-of-multiples/index.ts
+++ src/01-sum-of-multiples/index.ts
@@ -1,1 +1,9 @@
-export function sumOfMultiples(multiple, max) {}
+export function sumOfMultiples(multiple: number, max: number): number {
+  if (max < multiple) return 0
+  let sum: number = 0
+  for (let i = 1; i <= max; i++) {
+    if (i % multiple === 0) sum += i
+  }
+
+  return sum
+}

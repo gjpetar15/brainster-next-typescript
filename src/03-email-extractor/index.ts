781d59e35bb914cb98c3dfd392af02ed5c27864d
Petar Gjorgjioski <gjorgjioski.a22011@next.edu.mk>
2023-12-21 12:24:26 +0100

    treta vezba

--- src/03-email-extractor/index.ts
+++ src/03-email-extractor/index.ts
@@ -1,1 +1,17 @@
-export function extractEmails(users) {}
+type User = {
+  id: number
+  name: string
+  email: string
+}
+
+export function extractEmails(users: User[]): string[] {
+  const extractedEmails: string[] = []
+
+  for (const user of users) {
+    if (user && user.email && !extractedEmails.includes(user.email)) {
+      extractedEmails.push(user.email)
+    }
+  }
+
+  return extractedEmails
+}

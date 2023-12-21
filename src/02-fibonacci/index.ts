f97010bc65ad2333f62d399c752f1cb0885c8f16
Petar Gjorgjioski <gjorgjioski.a22011@next.edu.mk>
2023-12-21 12:19:46 +0100

    vtora vezba

--- src/02-fibonacci/index.ts
+++ src/02-fibonacci/index.ts
@@ -1,1 +1,16 @@
-export function fibonacci() {}
+export function fibonacci(n: number): number[] {
+  if (n === 0) return []
+  if (n === 1) return [0]
+
+  const fib: number[] = []
+
+  for (let i = 0; i < n; i++) {
+    if (i < 2) {
+      fib.push(i)
+      continue
+    }
+    fib.push(fib[i - 1] + fib[i - 2])
+  }
+
+  return fib
+}
